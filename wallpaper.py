19c50b12a7588e42f4c2d6ddc2dba2a4ef05907b
geonith-george <geonith98@gmail.com>
2020-02-05 23:15:24 +0530

    new api added

--- wallpaper.py
+++ wallpaper.py
@@ -1,10 +1,22 @@
 import random
 import urllib.request
 import os
+import requests
+import json
 
-number = random.randint(1,1000)
-url =  'https://i.picsum.photos/id/{0}/1920/1080.jpg'.format(number)
+nm = random.randint(1,7000)
 pth = os.getcwd()
+# url =  'https://i.picsum.photos/id/{0}/1920/1080.jpg'.format(number)
+
+def getLink(n):
+    r = requests.get(f'http://www.splashbase.co/api/v1/images/{n}')
+    try:
+        url2=json.loads(r.text)['url']
+        print(json.loads(r.text)['url'])
+        downloader(url2)
+    except:
+        print(".", end='')
+        getLink(random.randint(1,1000))
 
 def downloader(image_url):
     full_file_name = 'image.jpg'
@@ -13,11 +25,8 @@ def downloader(image_url):
 def change_wallpaper():
     string ="reg add \"HKEY_CURRENT_USER\\Control Panel\\Desktop\" /v Wallpaper /t REG_SZ /d {0}\\image.jpg /f".format(pth)
     os.system(string)
-    print(string)
     os.system("RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters")
-    os.system("del .\image.jpg")
-try:
-    downloader(url)
-    change_wallpaper()
-except:
-    pass
+    # os.system("del .\image.jpg")
+
+getLink(nm)
+change_wallpaper()
